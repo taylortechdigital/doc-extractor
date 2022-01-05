668aa33168312ae759d88c33990b03a529b93164
Thonda Taylor II <taylortechdigital@gmail.com>
2022-01-05 13:15:46 -0500

    Dirs Updates

---
+++ pdf/pdf-extractor.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const pdf = require('pdf-parse');
+
+//Traverse inputs directory
+
+
+//Parse existing pdf files
+const pdffile = fs.read
+
+
+//Update outputs directory
